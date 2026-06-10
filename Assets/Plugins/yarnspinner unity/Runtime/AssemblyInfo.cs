ef934d8785374727380e79e2505267b9392dc943
Tang Jin <156814864+tja688@users.noreply.github.com>
2026-06-10 11:26:21 +0800

    哎，还是导入yarn吧

---
+++ Assets/Plugins/yarnspinner unity/Runtime/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿/*
+Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("3.1.3.0")]
+[assembly: AssemblyFileVersion("3.1.3.0")]
+[assembly: AssemblyInformationalVersion("3.1.3.Branch.main.Sha.e1ba6f471d923270d2a0ba7b4f8ae81dc3f76adb")]
+[assembly: InternalsVisibleTo("YarnSpinnerTests")]
+[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
+
